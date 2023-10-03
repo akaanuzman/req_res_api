36441e272665d144001858ab832c73228c2b7b78
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-10-04 01:10:45 +0300

    initialize app

---
+++ android/app/src/main/kotlin/com/example/req_res_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.req_res_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/products/generation/colors.gen.dart
@@ -0,0 +1,21 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #F1E6FF
+  static const Color placeboPurple = Color(0xFFF1E6FF);
+
+  /// Color: #6F35A5
+  static const Color purplePristine = Color(0xFF6F35A5);
+}
